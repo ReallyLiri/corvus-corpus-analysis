c73824ba45b8de5c768da1a6a282fbc9cc0aa649
ReallyLiri <reallyliri@gmail.com>
2020-11-21 19:55:11 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, {useEffect, useState} from "react"
+import React, {useState} from "react"
 import BatAnimation from "./assets/img/idea-bat-animation.gif"
-import BgAnimation from "./assets/img/idea-bg-animation.gif"
 import Start from "./assets/img/idea-start.png"
 import CleanBackground from "./assets/img/idea-bg.png"
 import TitleBrief from "./assets/img/idea-title-1.png"
 import TitleMoodboard from "./assets/img/idea-title-2.png"
 import TitleMarket from "./assets/img/idea-title-3.png"
 import PageBrief from "./assets/img/idea-web-1.png"
+import PageMoodboard from "./assets/img/idea-web-2.png"
 import styled from "styled-components";
 
 const Height = window.innerHeight;
 const HeightRatio = Height / 1080.0;
 
 const StyledImage = styled.img`
+position: absolute;
   max-height: 100%;
   max-width: 100%;
+  z-index: ${props => props.z};
 `;
 
 const BatPointer = styled.div`
   position: absolute;
   top: ${props => HeightRatio * 350 + HeightRatio * 80 * props.pageIndex}px;
+  z-index: 3;
 `;
 
 const BatImage = styled.img`
@@ -31,6 +34,7 @@ const PageTitle = styled.div`
   position: absolute;
   top: ${props => HeightRatio * 320 + HeightRatio * 80 * props.pageIndex}px;
   cursor: pointer;
+  z-index: 3;
 `;
 
 const PageTitleImage = styled.img`
@@ -38,20 +42,8 @@ const PageTitleImage = styled.img`
   height: 100px;
 `;
 
-const Images = [
-    BatAnimation, BgAnimation, Start, CleanBackground, TitleBrief, TitleMoodboard, TitleMarket, PageBrief
-];
-
-const loadImage = path =>
-    new Promise(resolve => {
-        const img = new Image();
-        img.onload = () => resolve(path);
-        img.src = path;
-    });
-
 const App = () => {
 
-    const [isLoading, setLoading] = useState(true);
     const [started, setStarted] = useState(false);
     const [backgroundLoaded, setBackgroundLoaded] = useState(false);
     const [contentLoaded, setContentLoaded] = useState(false);
@@ -63,25 +55,11 @@ const App = () => {
         setTimeout(() => setContentLoaded(true), 6000);
     };
 
-    useEffect(() => {
-        Promise.all(Images.map(loadImage))
-            .then(
-                () => setLoading(false),
-                err => console.error(err)
-            )
-    }, []);
-
-    if (isLoading) {
-        return <div/>;
-    }
-
     return <React.Fragment>
         <div onClick={() => !started && start()} className={started ? "hidden" : "visible"}>
             <StyledImage src={Start} alt="start"/>
         </div>
-        {
-            !contentLoaded && <StyledImage src={CleanBackground} alt="background" className={backgroundLoaded ? "visible" : "hidden"}/>
-        }
+        <StyledImage z={1} src={CleanBackground} alt="background" className={backgroundLoaded ? "visible" : "hidden"}/>
         {
             contentLoaded && <BatPointer pageIndex={pageIndex}>
                 <BatImage src={BatAnimation} alt="bat"/>
@@ -95,14 +73,18 @@ const App = () => {
                 <PageTitle pageIndex={1} onClick={() => setPageIndex(1)}>
                     <PageTitleImage src={TitleMoodboard} alt="moodboard"/>
                 </PageTitle>
-                <PageTitle pageIndex={2}  onClick={() => setPageIndex(2)}>
+                <PageTitle pageIndex={2} onClick={() => setPageIndex(2)}>
                     <PageTitleImage src={TitleMarket} alt="market-research"/>
                 </PageTitle>
             </React.Fragment>
         }
         {
             contentLoaded &&
-            pageIndex >= 0 && <StyledImage src={PageBrief} alt="background"/>
+            pageIndex === 0 && <StyledImage z={2} src={PageBrief} alt="background"/>
+        }
+        {
+            contentLoaded &&
+            pageIndex === 1 && <StyledImage z={2} src={PageMoodboard} alt="background"/>
         }
     </React.Fragment>;
 };
