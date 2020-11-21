30361f486e0e9d99d108b1d51a4e8741e6ed1310
ReallyLiri <reallyliri@gmail.com>
2020-11-21 19:51:44 +0200

    Initial work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import BatAnimation from "./assets/img/idea-bat-animation.gif"
+import BgAnimation from "./assets/img/idea-bg-animation.gif"
+import Start from "./assets/img/idea-start.png"
+import CleanBackground from "./assets/img/idea-bg.png"
+import TitleBrief from "./assets/img/idea-title-1.png"
+import TitleMoodboard from "./assets/img/idea-title-2.png"
+import TitleMarket from "./assets/img/idea-title-3.png"
+import PageBrief from "./assets/img/idea-web-1.png"
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Height = window.innerHeight;
+const HeightRatio = Height / 1080.0;
+
+const StyledImage = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`;
+
+const BatPointer = styled.div`
+  position: absolute;
+  top: ${props => HeightRatio * 350 + HeightRatio * 80 * props.pageIndex}px;
+`;
+
+const BatImage = styled.img`
+  margin-left: 15px;
+  width: 100px;
+`;
+
+const PageTitle = styled.div`
+  position: absolute;
+  top: ${props => HeightRatio * 320 + HeightRatio * 80 * props.pageIndex}px;
+  cursor: pointer;
+`;
+
+const PageTitleImage = styled.img`
+  margin-left: 125px;
+  height: 100px;
+`;
+
+const Images = [
+    BatAnimation, BgAnimation, Start, CleanBackground, TitleBrief, TitleMoodboard, TitleMarket, PageBrief
+];
+
+const loadImage = path =>
+    new Promise(resolve => {
+        const img = new Image();
+        img.onload = () => resolve(path);
+        img.src = path;
+    });
+
+const App = () => {
+
+    const [isLoading, setLoading] = useState(true);
+    const [started, setStarted] = useState(false);
+    const [backgroundLoaded, setBackgroundLoaded] = useState(false);
+    const [contentLoaded, setContentLoaded] = useState(false);
+    const [pageIndex, setPageIndex] = useState(0);
+
+    const start = () => {
+        setStarted(true);
+        setTimeout(() => setBackgroundLoaded(true), 3000);
+        setTimeout(() => setContentLoaded(true), 6000);
+    };
+
+    useEffect(() => {
+        Promise.all(Images.map(loadImage))
+            .then(
+                () => setLoading(false),
+                err => console.error(err)
+            )
+    }, []);
+
+    if (isLoading) {
+        return <div/>;
+    }
+
+    return <React.Fragment>
+        <div onClick={() => !started && start()} className={started ? "hidden" : "visible"}>
+            <StyledImage src={Start} alt="start"/>
+        </div>
+        {
+            !contentLoaded && <StyledImage src={CleanBackground} alt="background" className={backgroundLoaded ? "visible" : "hidden"}/>
+        }
+        {
+            contentLoaded && <BatPointer pageIndex={pageIndex}>
+                <BatImage src={BatAnimation} alt="bat"/>
+            </BatPointer>
+        }
+        {
+            contentLoaded && <React.Fragment>
+                <PageTitle pageIndex={0} onClick={() => setPageIndex(0)}>
+                    <PageTitleImage src={TitleBrief} alt="brief"/>
+                </PageTitle>
+                <PageTitle pageIndex={1} onClick={() => setPageIndex(1)}>
+                    <PageTitleImage src={TitleMoodboard} alt="moodboard"/>
+                </PageTitle>
+                <PageTitle pageIndex={2}  onClick={() => setPageIndex(2)}>
+                    <PageTitleImage src={TitleMarket} alt="market-research"/>
+                </PageTitle>
+            </React.Fragment>
+        }
+        {
+            contentLoaded &&
+            pageIndex >= 0 && <StyledImage src={PageBrief} alt="background"/>
+        }
+    </React.Fragment>;
+};
 
 export default App;
